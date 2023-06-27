5e5de0d85c5f16ccb98cdc483fcef2262d67f762
Balaganesh K <k.balaganesh26@gmail.com>
2023-06-28 00:20:03 +0530

    added the drap and drop feature

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,14 @@ import Image from 'next/image';
 import { useState } from 'react';
 import TextBox from '@/components/TextBox';
 import { Inter } from 'next/font/google';
+import { resetServerContext } from 'react-beautiful-dnd';
 import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 
 const inter = Inter({
   subsets: ['latin'],
 });
 
-export default function Home() {
+export default function Home({ data }) {
   const [textBox1, setTextBox1] = useState(
     'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos?'
   );
@@ -21,59 +22,82 @@ export default function Home() {
 
   const contentList = [
     {
-      id: 1,
+      id: '11111111111',
       content: <TextBox value={textBox1} setValue={setTextBox1} />,
     },
     {
-      id: 2,
+      id: '22222222222222222',
       content: <TextBox value={textBox2} setValue={setTextBox2} />,
     },
     {
-      id: 3,
-      content: <TextBox value={textBox3} setValue={setTextBox3} />,
-    },
-    {
-      id: 3,
+      id: '333333333333333333333333',
       content: <TextBox value={textBox3} setValue={setTextBox3} />,
     },
   ];
 
+  const [content, setContent] = useState(contentList);
+
   const onDragEnd = (result) => {
-    console.log(result);
+    const { source, destination, type } = result;
+    if (!destination) {
+      return;
+    }
+
+    if (
+      source.droppableId === destination.droppableId &&
+      source.index === destination.index
+    ) {
+      return;
+    }
+
+    if (type == 'group') {
+      const newContentList = Array.from(contentList);
+      const [removed] = newContentList.splice(source.index, 1);
+      newContentList.splice(destination.index, 0, removed);
+
+      console.log(newContentList);
+      setContent(newContentList);
+    }
   };
 
   return (
     <main className={` ${inter.className} p-[4rem] flex`}>
-      <div className="bg-gray-500 flex-1 mr-[5rem] h-fit min-h-[calc(100vh-8rem)]">
+      <div className="bg-gray-500 flex-1 mr-[5rem] h-fit w-full min-h-[calc(100vh-8rem)]">
         <h1>Balaganesh</h1>
       </div>
-      <DragDropContext onDragEnd={onDragEnd}>
-        <Droppable droppableId="droppable-1">
-          {(provided) => (
-            <div
-              {...provided.droppableProps}
-              {...provided.innerRef}
-              className="flex flex-wrap w-full  max-w-[820px] h-full gap-[40px]">
-              {contentList.map((item, index) => (
-                <Draggable
-                  key={item.id}
-                  draggableId={`draggable-${item.id}`}
-                  index={index}>
-                  {(provided) => (
-                    <div
-                      className="h-fit"
-                      {...provided.dragHandleProps}
-                      {...provided.draggableProps}
-                      ref={provided.innerRef}>
-                      <div>{item.content}</div>
-                    </div>
-                  )}
-                </Draggable>
-              ))}
-            </div>
-          )}
-        </Droppable>
-      </DragDropContext>
+      <div className="max-w-[820px] w-[820px] min-h-[calc(100vh-8rem)]">
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Droppable droppableId="ROOT" type="group">
+            {(provided) => (
+              <div {...provided.droppableProps} ref={provided.innerRef}>
+                <div className="flex gap-[40px] flex-wrap">
+                  {content.map((item, index) => (
+                    <Draggable
+                      key={item.id}
+                      draggableId={item.id}
+                      index={index}>
+                      {(provided) => (
+                        <div
+                          ref={provided.innerRef}
+                          {...provided.dragHandleProps}
+                          {...provided.draggableProps}>
+                          <div>{item.content}</div>
+                        </div>
+                      )}
+                    </Draggable>
+                  ))}
+                </div>
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+      </div>
     </main>
   );
 }
+
+export const getServerSideProps = async ({ query }) => {
+  resetServerContext();
+
+  return { props: { data: [] } };
+};
