e42ce3a0e393f47cc70e7decb5b100b3c1663a92
Balaganesh K <k.balaganesh26@gmail.com>
2023-06-28 21:27:59 +0530

    updated the mapbox component

--- pages/mapbox.js
+++ pages/mapbox.js
@@ -16,8 +16,9 @@ const MapboxMap = () => {
   const [mapViewport, setMapViewport] = useState({
     longitude: 80.2337,
     latitude: 12.9915,
-    zoom: 12,
+    zoom: 8,
   });
+  const [isViewportFixed, setIsViewportFixed] = useState(true);
 
   const handleResize = (width, height) => {
     setWidth(width);
@@ -55,15 +56,32 @@ const MapboxMap = () => {
     searchLocation();
   }, [value, Token]);
 
+  useEffect(() => {
+    if (selectedLocation) {
+      setMapViewport({
+        longitude: selectedLocation.center[0],
+        latitude: selectedLocation.center[1],
+        zoom: 8,
+      });
+      setIsViewportFixed(true);
+      setInterval(() => {
+        setIsViewportFixed(false);
+      }, 1000);
+    } else {
+      setIsViewportFixed(false);
+    }
+  }, [selectedLocation]);
+
   const handleSelectLocation = (location) => {
     setSelectedLocation(location);
-    setMapViewport({
-      longitude: location.center[0],
-      latitude: location.center[1],
-      zoom: 12,
-    });
     setValue('');
     setSearchResults([]);
+  };
+
+  const handleViewportChange = (newViewport) => {
+    if (!selectedLocation) {
+      setMapViewport(newViewport);
+    }
   };
 
   return (
@@ -72,17 +90,21 @@ const MapboxMap = () => {
       height={height}
       key={renderKey}
       handleResize={handleResize}>
-      <div>
+      <div className="absolute bottom-[-3rem] left-0 z-[100] ">
         <input
+          className="w-[12rem] bg-black text-white p-2 rounded-lg placeholder:text-white"
           type="text"
           value={value}
           onChange={(e) => setValue(e.target.value)}
           placeholder="Search location"
         />
         {searchResults.length > 0 && (
-          <ul>
+          <ul className="absolute bottom-0 bg-black text-white mb-[2.75rem] ml-[5rem] max-h-[7rem] overflow-y-auto scrollbar-hide w-[12rem] p-2 rounded-lg ">
             {searchResults.map((result) => (
-              <li key={result.id} onClick={() => handleSelectLocation(result)}>
+              <li
+                className="truncate text-ellipsis p-1 cursor-pointer hover:bg-gray-700 rounded-lg"
+                key={result.id}
+                onClick={() => handleSelectLocation(result)}>
                 {result.place_name}
               </li>
             ))}
@@ -94,8 +116,8 @@ const MapboxMap = () => {
         height="100%"
         mapboxAccessToken={Token}
         mapStyle="mapbox://styles/mapbox/streets-v12"
-        {...mapViewport}
-        onViewportChange={setMapViewport}>
+        {...(isViewportFixed && { ...mapViewport })}
+        onViewportChange={handleViewportChange}>
         <NavigationControl />
         <GeolocateControl
           positionOptions={{ enableHighAccuracy: true }}
