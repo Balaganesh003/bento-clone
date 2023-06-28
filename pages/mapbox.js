0a81216c0a20fe427c98159f59a0fb48ee4e43b8
Balaganesh K <k.balaganesh26@gmail.com>
2023-06-28 20:16:46 +0530

    added the search feature

--- pages/mapbox.js
+++ pages/mapbox.js
@@ -10,6 +10,10 @@ const MapboxMap = () => {
   const [height, setHeight] = useState(1);
   const [renderKey, setRenderKey] = useState(0);
 
+  const [value, setValue] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [selectedLocation, setSelectedLocation] = useState(null);
+
   const handleResize = (width, height) => {
     setWidth(width);
     setHeight(height);
@@ -18,7 +22,39 @@ const MapboxMap = () => {
 
   const Token = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
 
-  useEffect(() => {}, [width, height]);
+  useEffect(() => {
+    const searchLocation = async () => {
+      if (value.trim() === '') {
+        setSearchResults([]);
+        return;
+      }
+
+      const geocodingUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+        value
+      )}.json?access_token=${Token}&autocomplete=true`;
+
+      try {
+        const response = await fetch(geocodingUrl);
+        const data = await response.json();
+
+        if (response.ok && data?.features?.length > 0) {
+          setSearchResults(data.features);
+        } else {
+          setSearchResults([]);
+        }
+      } catch (error) {
+        console.error('Error searching location:', error);
+      }
+    };
+
+    searchLocation();
+  }, [value, Token]);
+
+  const handleSelectLocation = (location) => {
+    setSelectedLocation(location);
+    setValue('');
+    setSearchResults([]);
+  };
 
   return (
     <ResizingContainer
@@ -26,6 +62,23 @@ const MapboxMap = () => {
       height={height}
       key={renderKey}
       handleResize={handleResize}>
+      <div>
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          placeholder="Search location"
+        />
+        {searchResults.length > 0 && (
+          <ul>
+            {searchResults.map((result) => (
+              <li key={result.id} onClick={() => handleSelectLocation(result)}>
+                {result.place_name}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
       <Map
         width="100%"
         height="100%"
@@ -33,16 +86,21 @@ const MapboxMap = () => {
         initialViewState={{
           longitude: 80.2337,
           latitude: 12.9915,
-          zoom: 12,
+          zoom: 14,
         }}
         mapStyle="mapbox://styles/mapbox/streets-v12">
         <GeolocateControl
           positionOptions={{ enableHighAccuracy: true }}
           trackUserLocation={true}
         />
-        <Marker longitude={80.2337} latitude={12.9915} anchor="bottom">
-          <FaMapMarkerAlt size={30} className="text-red-500" />
-        </Marker>
+        {selectedLocation && (
+          <Marker
+            longitude={selectedLocation.center[0]}
+            latitude={selectedLocation.center[1]}
+            anchor="bottom">
+            <FaMapMarkerAlt size={30} className="text-red-500" />
+          </Marker>
+        )}
       </Map>
     </ResizingContainer>
   );
