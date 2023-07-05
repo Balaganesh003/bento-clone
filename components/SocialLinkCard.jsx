f643bf06bfaabe9bfd91584df375252b4d8c4075
Balaganesh K <k.balaganesh26@gmail.com>
2023-07-05 20:41:56 +0530

    added the social link card

---
+++ components/SocialLinkCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ResizingContainer from './ResizingContainer';
+
+const SocialLinkCard = () => {
+  return (
+    <ResizingContainer>
+      <div>SocialLinkCard</div>;
+    </ResizingContainer>
+  );
+};
+
+export default SocialLinkCard;
