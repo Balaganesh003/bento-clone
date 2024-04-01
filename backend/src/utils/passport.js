86bb32ce16451e6f0ed3305fe093c8485f946d5e
Balaganesh K <k.balaganesh26@gmail.com>
2024-04-01 05:53:22 +0530

    added the passport config

---
+++ backend/src/utils/passport.js
@@ -0,0 +1,61 @@
+const passport = require('passport');
+const { Strategy: LocalStrategy } = require('passport-local');
+const { Strategy: GoogleStrategy } = require('passport-google-oauth20');
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: 'email',
+      passwordField: 'password',
+    },
+    async (email, password, done) => {
+      try {
+        const user = await User.findOne({ email });
+        if (!user) {
+          return done(null, false, { message: 'Invalid email or password' });
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+          return done(null, false, { message: 'Invalid email or password' });
+        }
+        return done(null, user);
+      } catch (err) {
+        return done(err);
+      }
+    }
+  )
+);
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: '/api/auth/google/callback',
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await User.findOne({ email: profile.emails[0].value });
+        if (!user) {
+          const newUser = new User({ email: profile.emails[0].value });
+          user = await newUser.save();
+        }
+        return done(null, user);
+      } catch (err) {
+        return done(err);
+      }
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
+});
