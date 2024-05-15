607c89821c73cf05677205f115a420d1ceee9447
Balaganesh K <k.balaganesh26@gmail.com>
2024-05-16 01:37:01 +0530

    refactor: Update authMiddleware to handle token verification and user authorization

--- backend/src/authMiddleware.js
+++ backend/src/authMiddleware.js
@@ -7,34 +7,36 @@ const authMiddleware = async (req, res, next) => {
     // Check for the authorization header
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
+    const urlUsername = req.params.username;
 
-    if (!token) {
-      console.log('Unauthorized: Missing token');
-      return res.status(401).json({ message: 'Unauthorized: Missing token' });
+    // Verify the token if present
+    if (token) {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const { userId, username } = decoded;
+
+      console.log('userId:', userId);
+      console.log('username:', username);
+      const user = await User.findById(userId);
+
+      if (!user) {
+        console.log('Unauthorized: User not found');
+        return res
+          .status(401)
+          .json({ message: 'Unauthorized: User not found' });
+      }
+
+      if (user.username !== username || username !== urlUsername) {
+        console.log('Unauthorized: Invalid token');
+
+        return res.status(401).json({ message: 'Unauthorized: Invalid token' });
+      }
+
+      // Attach the user object to the request for further processing
+      req.user = user;
     }
 
-    // Verify the token using the secret key
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    // Extract userId and username from the decoded token
-    const { userId, username } = decoded;
-
-    // Check if the user exists in the database
-    const user = await User.findById(userId);
-
-    if (!user) {
-      console.log('Unauthorized: User not found');
-      return res.status(401).json({ message: 'Unauthorized: User not found' });
-    }
-
-    if (user.username !== username) {
-      console.log('Unauthorized: Invalid token');
-      return res.status(401).json({ message: 'Unauthorized: Invalid token' });
-    }
-
-    // Attach the decoded user object to the request
-    req.user = user;
-    next(); // Call the next middleware or route handler
+    // Continue to the next middleware or route handler
+    next();
   } catch (error) {
     console.error('Token verification error:', error);
     return res.status(403).json({ message: 'Unauthorized: Invalid token' });
