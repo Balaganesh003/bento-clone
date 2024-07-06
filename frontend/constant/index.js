b15b634dc25c12acbd14ca8c0c7f6b777130d99d
Balaganesh K <k.balaganesh26@gmail.com>
2024-07-06 15:13:40 +0530

    feat: Update next.js dependency to version 14.2.4

---
+++ frontend/constant/index.js
@@ -0,0 +1,44 @@
+export const socialLinks = {
+  twitter: {
+    type: 'socialLink',
+    baseUrl: 'twitter',
+    userName: '',
+    logo: 'https://res.cloudinary.com/dqlj6jlir/image/upload/v1715698990/logo/twitter_leayqs.svg',
+    bgColor: '!bg-[#55ACEE] ',
+  },
+  instagram: {
+    type: 'socialLink',
+    baseUrl: 'instagram',
+    userName: '',
+    logo: 'https://res.cloudinary.com/dqlj6jlir/image/upload/v1715698960/logo/instagram_psrjkv.svg',
+    bgColor: '!bg-[#CE3B9F] ',
+  },
+  github: {
+    type: 'socialLink',
+    baseUrl: 'github',
+    userName: '',
+    logo: 'https://res.cloudinary.com/dqlj6jlir/image/upload/v1715698955/logo/github_bpmxzd.svg',
+    bgColor: '!bg-[#181717] ',
+  },
+  linkedin: {
+    type: 'socialLink',
+    baseUrl: 'linkedin',
+    userName: '',
+    logo: 'https://res.cloudinary.com/dqlj6jlir/image/upload/v1715698975/logo/linkedin_dmyqon.svg',
+    bgColor: '!bg-[#007EBB] ',
+  },
+  dribbble: {
+    type: 'socialLink',
+    baseUrl: 'dribbble',
+    userName: '',
+    logo: 'https://res.cloudinary.com/dqlj6jlir/image/upload/v1715699056/logo/dribble_wx1hht.svg',
+    bgColor: '!bg-[#D15584] ',
+  },
+  buymeacoffee: {
+    type: 'socialLink',
+    baseUrl: 'buymeacoffee',
+    userName: '',
+    logo: 'https://res.cloudinary.com/dqlj6jlir/image/upload/v1715699053/logo/coffee_eerped.svg',
+    bgColor: '!bg-[#FFDD06] ',
+  },
+};
